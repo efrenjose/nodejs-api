cbd3d5f6319a08bffbd061fef58cbff5d7b8117a
efrenrod <rodrigueza.efren@gmail.com>
2018-06-23 10:30:43 -0500

    create simple custom middleware for illustration purposes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const Joi = require('joi');
+const logger = require('./logger');
+const authenticate = rquire('./authenticate');
 const express = require('express');
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); //parses request object into json and sets req.body
+app.use(logger);
+app.use(authenticate);
 
 const courses = [
     { id: 1, name: 'course1' },

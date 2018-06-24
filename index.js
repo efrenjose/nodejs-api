11f918c780f37badc9820b6d9ecf704bd0dcb851
Efren Rodriguez <efrenrodriguez@Efrens-MacBook-Pro.local>
2018-06-24 12:01:45 -0500

    clear up index array

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
-const helmet = require('helmet');
-const morgan = require('morgan');
-const config = require('config');
-const startupDebugger = require('debug')('app:startup');
-const dbDebugger = require('debug')('app:db');
-const logger = require('./middleware/logger');
-const authenticate = require('./authentication');
-const home = require('./routes/home');
-const courses = require('./routes/genres')
+const Joi = require('joi');
+const genres = require('./routes/genres')
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', './views');
-
 app.use(express.json()); //parses request object into json and sets req.body
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(helmet());
-app.use('/', home);
-app.use('/api/genres', courses);
-
-//Config
-console.log('Application Name: ' + config.get('name'));
-console.log('Mail Server: ' + config.get('mail.host'));
-console.log('Mail Server password: ' + config.get('mail.password'));
-if (app.get('env') === 'development') {
-    app.use(morgan('dev'));
-    startupDebugger('Morgan enabled...');
-}
-app.use(logger);
-app.use(authenticate);
+app.use('/api/genres', genres);
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening in port ${port}...`));

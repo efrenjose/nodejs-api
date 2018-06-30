90db4183e9b5993ea932566adc5a39fae9beb3e5
efrenrod <rodrigueza.efren@gmail.com>
2018-06-30 09:56:35 -0500

    convert promises to async and await

--- async-demo/movies.js
+++ async-demo/movies.js
@@ -1,60 +1,48 @@
 
 console.log('Before');
 
-/*getCustomer(1, (customer) => {
+async function sendEmailAsync() {
+    
+    const customer = await getCustomer(1);
     console.log('Customer: ', customer);
+
     if (customer.isGold) {
-      getTopMovies((movies) => {
+        const movies = await getTopMovies();
         console.log('Top movies: ', movies);
-        sendEmail(customer.email, movies, () => {
-          console.log('Email sent...')
-        });
-      });
+        await sendEmail(customer.email, movies);
+        console.log('Email sent...')
     }
-  });*/
+}
 
-  getCustomer(1)
-    .then(customer => {
-        console.log('Customer: ', customer);
-        if (customer.isGold) {
-            getTopMovies()
-            .then(movies => {
-                console.log('Top movies: ', movies);
-                sendEmail(customer.email, movies)
-                .then(() => {
-                    console.log('Email sent...')
-                });
-            });
-        }
-    })
+sendEmailAsync();
 
-  console.log('After');
-  
-  function getCustomer(id) {
+console.log('After');
+
+function getCustomer(id) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-        resolve({ 
-            id: 1, 
-            name: 'Mosh Hamedani', 
-            isGold: true, 
-            email: 'email' 
-        });
-        }, 4000); 
-    }); 
-  }
-  
-  function getTopMovies() {
+            resolve({
+                id: 1,
+                name: 'Mosh Hamedani',
+                isGold: true,
+                email: 'email'
+            });
+        }, 4000);
+    });
+}
+
+function getTopMovies() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(['movie1', 'movie2']);
         }, 4000);
     });
-  }
-  
-  function sendEmail(email, movies) {
+}
+
+function sendEmail(email, movies) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve();
         }, 4000);
     });
-  }
+}

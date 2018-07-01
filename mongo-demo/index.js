93f3f0207a4898934a2f90ab8c7d5e758681f614
Efren Rodriguez <efrenrodriguez@Efrens-MacBook-Pro.local>
2018-07-01 16:05:12 -0500

    connect to mongodb and create a course document

---
+++ mongo-demo/index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log('Could not connect to MongoDB...', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.Now },
+    isPublished: Boolean
+});
+
+async function createCourse() {
+    const Course = mongoose.model('Course', courseSchema);
+    const course = new Course({
+    name: 'Angular Course',
+    author: 'Mosh',
+    tags: ['angular', 'frontend'],
+    isPublished: true
+});
+
+const result = await course.save();
+console.log(result);
+}
+
+createCourse();

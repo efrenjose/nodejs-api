95f4cafa2b7036766f9952e385fd449caa45f2ee
efrenjose <efrenrodriguez@live.com>
2018-07-08 13:13:12 -0500

    add more crud operations

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost/mongo-exercises')
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log('Could not connect to MongoDB...', err));
 
@@ -56,11 +56,60 @@ async function createCourse() {
     console.log(result);
 }
 
+// *** Update Course -  Query first *** //
+async function updateCourseQueryFirst(id) {
+    const course = await Course.findById(id);
+
+    if (!course) return;
+
+    course.isPublished = true;
+    course.author = 'Another Author';
+
+    const result = await course.save();
+    //console.log(result);
+
+    return result;
+}
+
+// *** Update Course -  Update first *** //
+async function updateCourseUpdateFirst(id) {
+    const result = await Course.update({ _id: id }, {
+        $set: {
+            author: 'Mosh',
+            isPublished: false
+        }
+    });
+
+    return result;
+}
+
+// *** Update Course -  Update first *** //
+async function updateCourseUpdateFirstFindByIdAndUpdate(id) {
+    const course = await Course.findByIdAndUpdate(id, {
+        $set: {
+            author: 'Jason',
+            isPublished: false
+        }
+    }, {new: true});
+
+    return course;
+}
+
+// *** Delete Course *** //
+async function removeCourse(id) {
+    const result = await Course.deleteOne({ _id: id });
+    return result;
+}
+
 async function run() {
-    const courses  = await getCourses();
-    const courses  = await getCoursesByPrice();
-    const courses  = await getCoursesRegEx();
-    const courses  = await createCourse();
+    //const courses = await getCourses();
+    //const courses = await getCoursesByPrice();
+    //const courses = await getCoursesRegEx();
+    //const courses = await createCourse();
+    //const courses = await updateCourseQueryFirst('5b424f7b08e7a90e89153d0a');
+    //const courses = await updateCourseUpdateFirst('5b424f7b08e7a90e89153d0a');
+    //const courses = await updateCourseUpdateFirstFindByIdAndUpdate('5b424f7b08e7a90e89153d0a');
+    const courses = await removeCourse('5b424f7b08e7a90e89153d0a');
     console.log(courses);
 }
 

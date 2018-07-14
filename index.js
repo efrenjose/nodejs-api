e8cdb9e7dc9a75ce0d9827371b4d2a9ab2352331
efrenjose <efrenrodriguez@live.com>
2018-07-14 10:23:09 -0500

    implement genres api with mongoose for persistance

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const genres = require('./routes/genres')
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(() => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json()); //parses request object into json and sets req.body
 app.use('/api/genres', genres);

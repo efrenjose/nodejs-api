74d12a05d783361758ba06a497dfff32a1ca79ad
efrenjose <efrenrodriguez@live.com>
2018-08-14 20:49:58 -0500

    model relationships using embedding and reference documents

---
+++ embedding.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const authorSchema = new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String
+});
+
+const Author = mongoose.model('Author', authorSchema);
+
+const Course = mongoose.model('Course', new mongoose.Schema({
+  name: String,
+  author: authorSchema
+}));
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name, 
+    author
+  }); 
+  
+  const result = await course.save();
+  console.log(result);
+}
+
+async function updateCourse(courseId) {
+    const course = await Course.findById(courseId);
+    course.author.name = "efren";
+    course.save();
+}
+
+async function listCourses() { 
+  const courses = await Course.find();
+  console.log(courses);
+}
+
+//createCourse('Node Course', new Author({ name: 'Mosh' }));
+
+updateCourse('5b4fd1df981d401571aa82d4');

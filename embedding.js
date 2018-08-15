b7a1337e01fd8b961d3d26853b98a92f609cf48e
efrenjose <efrenrodriguez@live.com>
2018-08-15 07:27:57 -0500

    add array of sub documents

--- embedding.js
+++ embedding.js
@@ -14,13 +14,13 @@ const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
-  author: authorSchema
+  authors: [authorSchema]
 }));
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
   const course = new Course({
     name, 
-    author
+    authors
   }); 
   
   const result = await course.save();
@@ -37,7 +37,26 @@ async function listCourses() {
   const courses = await Course.find();
   console.log(courses);
 }
+async function addAuthor(courseId, author) {
+  const course = await Course.findById(courseId);
+  course.authors.push(author);
+  course.save();
+}
 
-//createCourse('Node Course', new Author({ name: 'Mosh' }));
+async function removeAuthor(courseId, authorId) {
+  const course = await Course.findById(courseId);
+  const author = course.authors.id(authorId);
+  author.remove();
+  course.save();
+}
 
-updateCourse('5b4fd1df981d401571aa82d4');
+removeAuthor('5b738a43498a5fa30ea061b6', '5b738b70a11d3aa34e9807cc');
+
+//addAuthor('5b738a43498a5fa30ea061b6', new Author({ name: 'Efren'}));
+
+/*createCourse('Node Course', [
+  new Author({ name: 'Mosh' }),
+  new Author({ name: 'John  ' }),
+]);*/
+
+//updateCourse('5b4fd1df981d401571aa82d4');

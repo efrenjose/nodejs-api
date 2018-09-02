6663ca863a92896525ccd3bc4cc21e9d9148d260
efrenjose <efrenrodriguez@live.com>
2018-09-02 17:49:59 -0500

    move token generation to user module

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,4 @@
-const config = require('config');
 const _ = require('lodash');
-const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt');
 const Joi = require('joi');
 const { User } = require('../models/user');
@@ -18,8 +16,7 @@ router.post('/', async (req, res) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send('Invalid email or password');
 
-    const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
-    
+    const token = user.generateAuthToken();
     res.send(token);
 });
 

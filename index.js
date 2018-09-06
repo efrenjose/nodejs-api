a7df15d024a3cc4d9e595c1badb6c8d21afe6a61
efrenjose <efrenrodriguez@live.com>
2018-09-05 19:44:41 -0500

    Log unhandled rejections outside of the express context

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ process.on('uncaughtException', (ex) => {
     winston.error(ex.message, ex);
 })
 
+process.on('unhandledRejection', (ex) => {
+    console.log('WE GOT AN UNHANDLED REJECTION');
+    winston.error(ex.message, ex);
+})
+
 winston.add(winston.transports.File, { filename: 'logfile.log'});
 winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly', level: 'error' });
 

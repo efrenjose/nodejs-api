50316ff6620727e4b06f008c158618c8885065a9
efrenjose <efrenrodriguez@live.com>
2018-09-07 14:23:36 -0500

    add authorization to movies routes

--- routes/movies.js
+++ routes/movies.js
@@ -1,15 +1,17 @@
+const authorization = require('../middleware/authorization');
+const admin = require('../middleware/admin');
 const {Movie, Validate} = require('../models/movie'); 
 const {Genre} = require('../models/genres');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', authorization, async (req, res) => {
   const movies = await Movie.find().sort('name');
   res.send(movies);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', [authorization, admin], async (req, res) => {
   const { error } = Validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -30,7 +32,7 @@ router.post('/', async (req, res) => {
   res.send(movie);
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', [authorization, admin], async (req, res) => {
   const { error } = Validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -53,7 +55,7 @@ router.put('/:id', async (req, res) => {
   res.send(movie);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', [authorization, admin], async (req, res) => {
   const movie = await Movie.findByIdAndRemove(req.params.id);
 
   if (!movie) return res.status(404).send('The movie with the given ID was not found.');
@@ -61,7 +63,7 @@ router.delete('/:id', async (req, res) => {
   res.send(movie);
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', authorization, async (req, res) => {
   const movie = await Movie.findById(req.params.id);
 
   if (!movie) return res.status(404).send('The movie with the given ID was not found.');

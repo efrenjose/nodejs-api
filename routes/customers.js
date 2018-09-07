0a6977ffe5f7df30941e2eb43c026090bf7d9458
efrenjose <efrenrodriguez@live.com>
2018-09-07 14:22:04 -0500

    add authorization to customers routes

--- routes/customers.js
+++ routes/customers.js
@@ -1,13 +1,15 @@
+const authorization = require('../middleware/authorization');
+const admin = require('../middleware/admin');
 const { Customer, validate} = require('../models/customer');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', authorization, async (req, res) => {
   const customers = await Customer.find().sort('name');
   res.send(customers);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', [authorization, admin], async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -21,7 +23,7 @@ router.post('/', async (req, res) => {
   res.send(customer);
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', [authorization, admin], async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -37,7 +39,7 @@ router.put('/:id', async (req, res) => {
   res.send(customer);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', [authorization, admin], async (req, res) => {
   const customer = await Customer.findByIdAndRemove(req.params.id);
 
   if (!customer) return res.status(404).send('The customer with the given ID was not found.');
@@ -45,7 +47,7 @@ router.delete('/:id', async (req, res) => {
   res.send(customer);
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', authorization, async (req, res) => {
   const customer = await Customer.findById(req.params.id);
 
   if (!customer) return res.status(404).send('The customer with the given ID was not found.');

5e6070e207259595b25fd3669d41cefa66c8e86a
efrenjose <efrenrodriguez@live.com>
2018-09-22 17:13:40 -0500

    move routes and db code to startup folder

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 require('express-async-errors');
 const winston = require('winston');
 require('winston-mongodb');
-const error = require('./middleware/error')
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const genres = require('./routes/genres')
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const authentication = require('./routes/authentication');
-const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+
+require('./startup/routes')(app);
+require('./startup/db')();
 
 //Log uncaught exceptions outside of the express context
 process.on('uncaughtException', (ex) => {
     winston.error(ex.message, ex);
     process.exit(1);
 })
-
-
 
 //Log unhandled rejections outside of the express context
 process.on('unhandledRejection', (ex) => {
@@ -37,19 +30,7 @@ if (!config.get('jwtPrivateKey')) {
     process.exit(1);
 }
 
-mongoose.connect('mongodb://localhost/vidly')
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(() => console.error('Could not connect to MongoDB...'));
 
-app.use(express.json()); //parses request object into json and sets req.body
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', authentication);
-
-app.use(error);
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening in port ${port}...`));
